c7bc1f26c6f1b1b0c081054f962a48f9588f906e
Skrrainer <krainer@outlook.com.br>
2025-08-14 16:49:18 -0400

    FEAT: Adiciona quatro novos arquivos referentes as aulas do Módulo 'Variáveis e constantes'.

---
+++ JavaScript/Variáveis e Constantes/variables.js
@@ -0,0 +1,23 @@
+ // Declara uma variavel sem valor
+ var userVar
+ let userLet
+ //Valor é fixo, não pode ser alterado
+ const userFixed = 
+
+ console.log(userVar)
+ console.log(userLet)
+ console.log(userFixed)
+
+
+
+
+ // Declara uma variavel com valor
+ var emailVar = "felipekrainer@hotmail.com"
+ let emailLet = "felipekrainer@hotmail.com"
+ console.log(emailVar)
+ console.log(emailLet)
+
+ //Substituir o valor da variavel
+ emailVar = "krainer@outlook.com.br"
+ console.log(emailVar)
+
