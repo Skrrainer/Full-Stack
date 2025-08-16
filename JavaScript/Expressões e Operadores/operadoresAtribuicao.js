c517ede5eb69cb542da484b34f6b99dcc2a3fa7e
Skrrainer <krainer@outlook.com.br>
2025-08-15 20:03:17 -0400

    FEAT: Adiciona quatro novos arquivos referentes ao Módulo 'Expressões e Operadores'.

---
+++ JavaScript/Expressões e Operadores/operadoresAtribuicao.js
@@ -0,0 +1,30 @@
+let value
+
+//Operador atribuição
+value = 1
+console.log(value)
+
+//Incremento
+value+= 2
+console.log(value)
+
+//Decremento
+value-= 2
+console.log(value)
+
+// Multiplicação
+value *= 2
+console.log(value)
+
+// Divisão
+value /= 2
+console.log(value)
+
+// Resto
+value %= 2
+console.log(value)
+
+// Exponenciacao
+value **= 2
+console.log(value)
+
---
+++ JavaScript/Expressões e Operadores/ordemDePrecedencia.js
@@ -0,0 +1,8 @@
+let num1 = 2 + 3 * 4
+console.log(num1)
+
+let num2 = (2 + 3) * 4
+console.log(num2)
+
+let avg = (9.5 + 7 + 5) / 3
+console.log(avg)
